8cab6fc2c118de6e61e09e04e9a6c68595ca773d
宋小伟 <songxiaowei@songxiaoweideMacBook-Pro.local>
2022-09-29 23:07:45 +0800

    adptive layout test project

---
+++ RockScissorPaper/RockScissorPaper/InfoViewController.swift
@@ -0,0 +1,33 @@
+//
+//  InfoViewController.swift
+//  RockScissorPaper
+//
+//  Created by 宋小伟 on 2022/9/29.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+
+    @IBAction func backMain(_ sender: Any) {
+        
+        dismiss(animated: true)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
